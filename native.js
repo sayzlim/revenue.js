30041db0c938b88464108f8f3ac2b41902aac02e
Sayz Lim <talk@sayzlim.net>
2018-09-01 17:01:54 +0700

    Use module pattern
    Adhere to standardjs style guide and simplify repetitive variables.

--- native.js
+++ native.js
@@ -1,116 +1,134 @@
-var _native = {
-  construct: function(e) {
-    var default_options = {
-      carbonZoneKey: "",
-      display: "block",
-      fallback: "",
-      ignore: "false",
-      placement: "nativejs",
-      prefix: "native",
-      targetClass: "native-ad",
-      visibleClass: "native-show"
-    };
+var _native = (function () {
+  var _options = {}
+  var _construct = function (e) {
+    var defaultOptions = {
+      carbonZoneKey: '',
+      display: 'block',
+      fallback: '',
+      ignore: 'false',
+      placement: '',
+      prefix: 'native',
+      targetClass: 'native-ad',
+      visibleClass: 'native-show'
+    }
 
-    if (typeof e == "undefined") return default_options;
-    Object.keys(default_options).forEach((key, index) => {
-      if (typeof e[key] == "undefined") {
-        e[key] = default_options[key];
+    if (typeof e === 'undefined') return defaultOptions
+    Object.keys(defaultOptions).forEach((key, index) => {
+      if (typeof e[key] === 'undefined') {
+        e[key] = defaultOptions[key]
       }
-    });
-    return e;
-  },
-  init: function(zone, options) {
-    options = this.construct(options);
-    this.carbonZoneKey = options["carbonZoneKey"];
-    this.targetClass = options["targetClass"];
-    this.displayStyle = options["display"];
-    this.fallback = options["fallback"];
-    this.ignore = options["ignore"];
-    this.visibleClassName = options["visibleClass"];
-    this.prefix = options["prefix"];
-    this.placement = options["placement"];
+    })
+    return e
+  }
 
-    let jsonUrl = `https://srv.buysellads.com/ads/${zone}.json?callback=_native_go`;
-    if (options["placement"] !== "") {
-      jsonUrl += "&segment=placement:" + options["placement"];
+  var init = function (zone, options) {
+    _options = _construct(options)
+
+    let jsonUrl = `https://srv.buysellads.com/ads/${zone}.json?callback=_native_go`
+    if (_options['placement'] !== '') {
+      jsonUrl += '&segment=placement:' + _options['placement']
     }
-    if (options["ignore"] == "true") {
-      jsonUrl += "&ignore=yes"
+    if (_options['ignore'] === 'true') {
+      jsonUrl += '&ignore=yes'
     }
 
-    let srv = document.createElement("script");
-    srv.src = jsonUrl;
-    document.getElementsByTagName("head")[0].appendChild(srv);
-  },
-  carbonFallback: function() {
-    let carbonScript = document.createElement("script");
-    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=" + _native.carbonZoneKey + "&placement=" + _native.placement;
-    carbonScript.id = "_carbonads_js";
+    let srv = document.createElement('script')
+    srv.src = jsonUrl
+    document.getElementsByTagName('head')[0].appendChild(srv)
+  }
 
-    return carbonScript;
-  },
-  sanitize: function(ads) {
+  var carbon = function (e) {
+    let srv = document.createElement('script')
+    srv.src = '//cdn.carbonads.com/carbon.js?serve=' + e['carbonZoneKey'] + '&placement=' + e['placement']
+    srv.id = '_carbonads_js'
+
+    return srv
+  }
+
+  var sanitize = function (ads) {
     return ads
       .filter(ad => {
-        return Object.keys(ad).length > 0;
+        return Object.keys(ad).length > 0
       })
       .filter(ad => {
-        return ad.hasOwnProperty("statlink");
-      });
-  },
-  pixel: function(p, timestamp) {
-    let c = "";
-    if (p)
-      p.split("||").forEach((pixel, index) => {
-        c += '<img src="' + pixel.replace("[timestamp]", timestamp) + '" style="display:none;" height="0" width="0" />';
-      });
-    return c;
+        return ad.hasOwnProperty('statlink')
+      })
   }
-};
 
-var _native_go = function(json) {
-  let ads = _native.sanitize(json["ads"]);
+  var pixel = function (p, timestamp) {
+    let c = ''
+    if (p) {
+      p.split('||').forEach((pixel, index) => {
+        c += `<img src="${pixel.replace('[timestamp]', timestamp)}" style="display:none;" height="0" width="0" />`
+      })
+    }
+    return c
+  }
+
+  var options = function () {
+    return _options
+  }
+
+  return {
+    carbon: carbon,
+    init: init,
+    options: options,
+    pixel: pixel,
+    sanitize: sanitize
+  }
+})({})
+
+var _native_go = function (json) {
+  let options = _native.options()
+  let ads = _native.sanitize(json['ads'])
+  let selectedClass = document.querySelectorAll('.' + options['targetClass'])
 
   if (ads.length < 1) {
-    document.querySelectorAll("." + _native.targetClass).forEach((className, index) => {
-      document.getElementsByClassName(_native.targetClass)[index].innerHTML = _native.fallback;
-      if (_native.fallback !== "" || _native.carbonZoneKey !== "") document.getElementsByClassName(_native.targetClass)[index].className += " " + _native.visibleClassName;
-      if (_native.carbonZoneKey !== "") document.getElementsByClassName(_native.targetClass)[index].appendChild(_native.carbonFallback());
-    });
+    selectedClass.forEach((className, index) => {
+      let selectedTarget = document.getElementsByClassName(options['targetClass'])[index]
 
-    return "No ads found";
+      if (options['fallback'] !== '' || options['carbonZoneKey'] !== '') selectedTarget.className += ' ' + options['visibleClass']
+      selectedTarget.innerHTML = options['fallback']
+      if (options['carbonZoneKey'] !== '') selectedTarget.appendChild(_native.carbon(options))
+    })
+
+    // End at this line if no ads are found, avoiding unnecessary steps
+    return
   }
 
-  document.querySelectorAll("." + _native.targetClass).forEach((className, index) => {
-    if (ads[index] && className) {
-      let adElement = document.getElementsByClassName(_native.targetClass)[index].innerHTML;
+  selectedClass.forEach((className, index) => {
+    let selectedTarget = document.getElementsByClassName(options['targetClass'])[index]
+    let adElement = selectedTarget.innerHTML
+    let prefix = options['prefix']
+    let ad = ads[index]
 
-      let ad = adElement
-        .replace(new RegExp("#" + _native.prefix + "_bg_color#", "g"), ads[index]["backgroundColor"])
-        .replace(new RegExp("#" + _native.prefix + "_bg_color_hover#", "g"), ads[index]["backgroundHoverColor"])
-        .replace(new RegExp("#" + _native.prefix + "_company#", "g"), ads[index]["company"])
-        .replace(new RegExp("#" + _native.prefix + "_cta#", "g"), ads[index]["callToAction"])
-        .replace(new RegExp("#" + _native.prefix + "_cta_bg_color#", "g"), ads[index]["ctaBackgroundColor"])
-        .replace(new RegExp("#" + _native.prefix + "_cta_bg_color_hover#", "g"), ads[index]["ctaBackgroundHoverColor"])
-        .replace(new RegExp("#" + _native.prefix + "_cta_color#", "g"), ads[index]["ctaTextColor"])
-        .replace(new RegExp("#" + _native.prefix + "_cta_color_hover#", "g"), ads[index]["ctaTextColorHover"])
-        .replace(new RegExp("#" + _native.prefix + "_desc#", "g"), ads[index]["description"])
-        .replace(new RegExp("#" + _native.prefix + "_index#", "g"), _native.prefix + "-" + ads[index]["i"])
-        .replace(new RegExp("#" + _native.prefix + "_img#", "g"), ads[index]["image"])
-        .replace(new RegExp("#" + _native.prefix + "_small_img#", "g"), ads[index]["smallImage"])
-        .replace(new RegExp("#" + _native.prefix + "_link#", "g"), ads[index]["statlink"])
-        .replace(new RegExp("#" + _native.prefix + "_logo#", "g"), ads[index]["logo"])
-        .replace(new RegExp("#" + _native.prefix + "_color#", "g"), ads[index]["textColor"])
-        .replace(new RegExp("#" + _native.prefix + "_color_hover#", "g"), ads[index]["textColorHover"])
-        .replace(new RegExp("#" + _native.prefix + "_title#", "g"), ads[index]["title"]);
+    if (ad && className) {
+      let adInnerHtml = adElement
+        .replace(new RegExp('#' + prefix + '_bg_color#', 'g'), ad['backgroundColor'])
+        .replace(new RegExp('#' + prefix + '_bg_color_hover#', 'g'), ad['backgroundHoverColor'])
+        .replace(new RegExp('#' + prefix + '_company#', 'g'), ad['company'])
+        .replace(new RegExp('#' + prefix + '_cta#', 'g'), ad['callToAction'])
+        .replace(new RegExp('#' + prefix + '_cta_bg_color#', 'g'), ad['ctaBackgroundColor'])
+        .replace(new RegExp('#' + prefix + '_cta_bg_color_hover#', 'g'), ad['ctaBackgroundHoverColor'])
+        .replace(new RegExp('#' + prefix + '_cta_color#', 'g'), ad['ctaTextColor'])
+        .replace(new RegExp('#' + prefix + '_cta_color_hover#', 'g'), ad['ctaTextColorHover'])
+        .replace(new RegExp('#' + prefix + '_desc#', 'g'), ad['description'])
+        .replace(new RegExp('#' + prefix + '_index#', 'g'), prefix + '-' + ad['i'])
+        .replace(new RegExp('#' + prefix + '_img#', 'g'), ad['image'])
+        .replace(new RegExp('#' + prefix + '_small_img#', 'g'), ad['smallImage'])
+        .replace(new RegExp('#' + prefix + '_link#', 'g'), ad['statlink'])
+        .replace(new RegExp('#' + prefix + '_logo#', 'g'), ad['logo'])
+        .replace(new RegExp('#' + prefix + '_color#', 'g'), ad['textColor'])
+        .replace(new RegExp('#' + prefix + '_color_hover#', 'g'), ad['textColorHover'])
+        .replace(new RegExp('#' + prefix + '_title#', 'g'), ad['title'])
 
-      document.getElementsByClassName(_native.targetClass)[index].innerHTML = null;
-      document.getElementsByClassName(_native.targetClass)[index].innerHTML += ad + _native.pixel(ads[index]["pixel"], ads[index]["timestamp"]);
-      document.getElementsByClassName(_native.targetClass)[index].style.display = _native.displayStyle;
-      if (_native.targetClass !== "") document.getElementsByClassName(_native.targetClass)[index].className += " " + _native.visibleClassName;
+      selectedTarget.innerHTML = null
+      selectedTarget.innerHTML += adInnerHtml + _native.pixel(ad['pixel'], ad['timestamp'])
+      selectedTarget.style.display = options['display']
+      if (options['targetClass'] !== '') selectedTarget.className += ' ' + options['visibleClass']
     } else {
-      document.getElementsByClassName(_native.targetClass)[index].innerHTML = null;
-      document.getElementsByClassName(_native.targetClass)[index].style.display = "none";
+      selectedTarget.innerHTML = null
+      selectedTarget.style.display = 'none'
     }
-  });
-};
+  })
+}
